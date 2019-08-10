cb9b68a0c9114800a8d44a515d577ce8d22bec5e
skykoradji <rongguiyou@gmail.com>
2019-08-10 21:06:51 +1000

    add images

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import Courses from '../components/Courses';
 import withLayout from '../lib/withLayout';
 
-import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
   headline: {
@@ -16,23 +16,20 @@ const styles = {
   }
 };
 
-class Home extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <Typography className={classes.headline} variant="h4" component="h4">
-          Development
-        </Typography>
-        <Typography className={classes.subtitle} variant="h6" component="p">
-          Get the Technology and Creative Skills you need to launch a career by learning from
-          industry experts.
-        </Typography>
+function Home({ classes, history }) {
+  return (
+    <div>
+      <Typography className={classes.headline} variant="h4" component="h4">
+        Development
+      </Typography>
+      <Typography className={classes.subtitle} variant="h6" component="p">
+        Get the Technology and Creative Skills you need to launch a career by learning from
+        industry experts.
+      </Typography>
 
-        <Courses history={this.props.history} />
-      </div>
-    );
-  }
+      <Courses history={history} />
+    </div>
+  );
 }
 
 export default withLayout(withStyles(styles)(Home));
